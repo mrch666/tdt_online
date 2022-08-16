c303b166faa24fa47a60d923846b77600020c3a4
Alex Churin <qitip@yandex.ru>
2022-08-16 16:40:18 +0300

    add libudfdll.so add Readme.md edit connection string and test testing debian 3 add new query models in storage add api routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 
+login = LoginManager()
+login.login_view = 'auth.login'
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
@@ -12,6 +15,7 @@ bootstrap = Bootstrap()
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    login.init_app(app)
     db.init_app(app)
     bootstrap.init_app(app)
     from app.api import bp as api_bp
