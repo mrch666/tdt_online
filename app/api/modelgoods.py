4318a6f1b2d1c3b4c68acd7be820d146646008de
Alex Churin <qitip@yandex.ru>
2022-08-16 16:49:17 +0300

    add libudfdll.so add Readme.md edit connection string and test testing debian 3 add new query models in storage add api routes 4

--- app/api/modelgoods.py
+++ app/api/modelgoods.py
@@ -3,10 +3,10 @@ from flask import jsonify
 from app.api import bp
 from app.models import Modelgood
 
-@bp.route('/modelgoods/<str:id>', methods=['GET'])
+@bp.route('/modelgoods/<string:id>', methods=['GET'])
 def get_model_by_id(id):
     return jsonify(Modelgood.query.get_or_404(id).to_dict())
 
-@bp.route('/modelgoods/search/<str:searchtext>', methods=['GET'])
+@bp.route('/modelgoods/search/<string:searchtext>', methods=['GET'])
 def get_models_by_id(searchtext):
     return jsonify(Modelgood.query.get_or_404(searchtext).to_dict())
