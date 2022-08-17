e0f9bb426f14a41a4c5e0aec78588315e791dbee
Alex Churin <qitip@yandex.ru>
2022-08-18 00:03:33 +0300

    add libudfdll.so add Readme.md edit connection string and test testing debian 3 add new query models in storage add api routes , modelgooids search 2 add module main by bp add serializer to models.py add search Uppercase filtered storages

--- app/api/modelgoods.py
+++ app/api/modelgoods.py
@@ -15,6 +15,7 @@ def get_model_by_id(id):
 
 @bp.route('/modelgoods/search/<string:searchtext>', methods=['GET'])
 def get_models_by_id(searchtext):
+  if len(searchtext)>3:
     basequery=db.session.query(Storage,Modelgood, Vollink,Vol,Folder).\
             join(Modelgood,Storage.modelid == Modelgood.id).\
             join(Folder, Storage.folderid == Folder.id).\
@@ -25,11 +26,13 @@ def get_models_by_id(searchtext):
             filter(Storage.folderid != '0rfarg000FZh')
     if " " in searchtext:
      for search in searchtext.split(' '):
-         search_args = [col.ilike('%%%s%%' % search) for col in [Modelgood.name, Vollink.barcode]]
-         basequery=basequery.filter(or_(*search_args))
+         if len(search)>2:
+             search_args = [col.ilike('%%%s%%' % search) for col in [Modelgood.name, Vollink.barcode]]
+             basequery=basequery.filter(or_(*search_args))
     else:
-        search_args=[col.ilike('%%%s%%' % searchtext) for col in [Modelgood.name, Vollink.barcode]]
-        basequery = basequery.filter(or_(*search_args))
+        if len(searchtext) > 3:
+            search_args=[col.ilike('%%%s%%' % searchtext) for col in [Modelgood.name, Vollink.barcode]]
+            basequery = basequery.filter(or_(*search_args))
     list=[(s.to_dict(),m.to_dict(),v.to_dict(),vl.to_dict(),fl.to_dict(),
            {"imageurl":"http://"+config.Config.serverdb+'''/img/'''+m.imagename() if m.imagename() else None}) for s,m,v,vl,fl in
         basequery.\
