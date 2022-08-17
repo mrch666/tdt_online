d134c459ed0314e4bd9f1feab4c1f0cd6b46fe09
Alex Churin <qitip@yandex.ru>
2022-08-17 13:36:49 +0300

    add libudfdll.so add Readme.md edit connection string and test testing debian 3 add new query models in storage add api routes , modelgooids search 2 add module main by bp add serializer to models.py add search Uppercase filtered storages

--- app/api/modelgoods.py
+++ app/api/modelgoods.py
@@ -1,4 +1,5 @@
 from flask import jsonify
+from sqlalchemy import or_
 from sqlalchemy.orm import Bundle
 
 from app.api import bp
@@ -13,6 +14,7 @@ def get_model_by_id(id):
 
 @bp.route('/modelgoods/search/<string:searchtext>', methods=['GET'])
 def get_models_by_id(searchtext):
+    search_args=[col.ilike('%%%s%%' % searchtext) for col in ['Modelgood.name', 'Vollink.barcode']]
     list=[(s.to_dict(),m.to_dict(),v.to_dict(),vl.to_dict(),fl.to_dict()) for s,m,v,vl,fl in db.session.query(Storage,Modelgood, Vollink,Vol,Folder).\
         join(Modelgood,Storage.modelid == Modelgood.id).\
         join(Folder, Storage.folderid == Folder.id).\
@@ -21,6 +23,7 @@ def get_models_by_id(searchtext):
         filter(Vollink.level=='1'). \
         filter(Storage.folderid != '0rfarg000os1'). \
         filter(Storage.folderid != '0rfarg000FZh'). \
-        filter(Modelgood.name.ilike(f'%{searchtext}%')).\
+        filter(or_(*search_args)).\
+        # filter(Modelgood.name.ilike(f'%{searchtext}%')).\
                    all()]
     return jsonify(list)
