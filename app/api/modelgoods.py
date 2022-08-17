fffc0f1c8d8c2252e0efebd801ce9acd6edaa582
Alex Churin <qitip@yandex.ru>
2022-08-17 13:39:16 +0300

    add libudfdll.so add Readme.md edit connection string and test testing debian 3 add new query models in storage add api routes , modelgooids search 2 add module main by bp add serializer to models.py add search Uppercase filtered storages

--- app/api/modelgoods.py
+++ app/api/modelgoods.py
@@ -14,7 +14,7 @@ def get_model_by_id(id):
 
 @bp.route('/modelgoods/search/<string:searchtext>', methods=['GET'])
 def get_models_by_id(searchtext):
-    search_args=[col.ilike('%%%s%%' % searchtext) for col in ['Modelgood.name', 'Vollink.barcode']]
+    search_args=[col.ilike('%%%s%%' % searchtext) for col in [Modelgood.name, Vollink.barcode]]
     list=[(s.to_dict(),m.to_dict(),v.to_dict(),vl.to_dict(),fl.to_dict()) for s,m,v,vl,fl in db.session.query(Storage,Modelgood, Vollink,Vol,Folder).\
         join(Modelgood,Storage.modelid == Modelgood.id).\
         join(Folder, Storage.folderid == Folder.id).\
