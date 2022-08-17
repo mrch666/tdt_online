1e5c4982e25af14f6b06fa5ef8f2c375505ab2a1
Alex Churin <qitip@yandex.ru>
2022-08-17 23:11:10 +0300

    add libudfdll.so add Readme.md edit connection string and test testing debian 3 add new query models in storage add api routes , modelgooids search 2 add module main by bp add serializer to models.py add search Uppercase filtered storages

--- app/api/modelgoods.py
+++ app/api/modelgoods.py
@@ -15,17 +15,24 @@ def get_model_by_id(id):
 
 @bp.route('/modelgoods/search/<string:searchtext>', methods=['GET'])
 def get_models_by_id(searchtext):
-    search_args=[col.ilike('%%%s%%' % searchtext) for col in [Modelgood.name, Vollink.barcode]]
+    basequery=db.session.query(Storage,Modelgood, Vollink,Vol,Folder).\
+            join(Modelgood,Storage.modelid == Modelgood.id).\
+            join(Folder, Storage.folderid == Folder.id).\
+            join(Vollink,Modelgood.id==Vollink.modelid).\
+            join(Vol, Vollink.vol1id==Vol.id).\
+            filter(Vollink.level=='1'). \
+            filter(Storage.folderid != '0rfarg000os1'). \
+            filter(Storage.folderid != '0rfarg000FZh')
+    if " " in searchtext:
+     for search in searchtext.split(' '):
+         search_args = [col.ilike('%%%s%%' % search) for col in [Modelgood.name, Vollink.barcode]]
+         basequery=basequery.filter(or_(*search_args))
+    else:
+        search_args=[col.ilike('%%%s%%' % searchtext) for col in [Modelgood.name, Vollink.barcode]]
+        basequery = basequery.filter(or_(*search_args))
     list=[(s.to_dict(),m.to_dict(),v.to_dict(),vl.to_dict(),fl.to_dict(),
-           {"imageurl":"http://"+config.Config.serverdb+'''/img/'''+m.imagename() if m.imagename() else None}) for s,m,v,vl,fl in db.session.query(Storage,Modelgood, Vollink,Vol,Folder).\
-        join(Modelgood,Storage.modelid == Modelgood.id).\
-        join(Folder, Storage.folderid == Folder.id).\
-        join(Vollink,Modelgood.id==Vollink.modelid).\
-        join(Vol, Vollink.vol1id==Vol.id).\
-        filter(Vollink.level=='1'). \
-        filter(Storage.folderid != '0rfarg000os1'). \
-        filter(Storage.folderid != '0rfarg000FZh'). \
-        filter(or_(*search_args)).\
+           {"imageurl":"http://"+config.Config.serverdb+'''/img/'''+m.imagename() if m.imagename() else None}) for s,m,v,vl,fl in
+        basequery.\
         # filter(Modelgood.name.ilike(f'%{searchtext}%')).\
                    all()]
     return jsonify(list)
