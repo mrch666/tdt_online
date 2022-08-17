2812024cc92c3d7bca1fca6324f3a43d12c7de4e
Alex Churin <qitip@yandex.ru>
2022-08-17 10:31:57 +0300

    add libudfdll.so add Readme.md edit connection string and test testing debian 3 add new query models in storage add api routes , modelgooids search add module main by bp

--- app/api/modelgoods.py
+++ app/api/modelgoods.py
@@ -2,7 +2,8 @@ from flask import jsonify
 
 from app.api import bp
 from app import db
-from app.models import Modelgood
+from app.models import Modelgood, Storage, Vollink, Vol, Folder
+
 
 @bp.route('/modelgoods/<string:id>', methods=['GET'])
 def get_model_by_id(id):
@@ -10,4 +11,11 @@ def get_model_by_id(id):
 
 @bp.route('/modelgoods/search/<string:searchtext>', methods=['GET'])
 def get_models_by_id(searchtext):
-    return jsonify(db.session.query(Modelgood).get_or_404(searchtext).to_dict())
+    return jsonify(db.session.query(Storage,Modelgood, Vollink,Vol,Folder).\
+        join(Modelgood,Storage.modelid == Modelgood.id).\
+        join(Folder, Storage.folderid == Folder.id).\
+        join(Vollink,Modelgood.id==Vollink.modelid).\
+        join(Vol, Vollink.vol1id==Vol.id).\
+        filter(Vollink.level=='1'). \
+                   filter(Modelgood.name.like(f'%{searchtext}%')). \
+                   items)
