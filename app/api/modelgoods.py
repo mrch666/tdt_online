088694763584edf5a7c3979235c916fe523f192f
Alex Churin <qitip@yandex.ru>
2022-08-18 14:07:38 +0300

    add libudfdll.so add Readme.md edit connection string and test testing debian 3 add new query models in storage add api routes , modelgooids search 2 add module main by bp add serializer to models.py add search Uppercase filtered storages

--- app/api/modelgoods.py
+++ app/api/modelgoods.py
@@ -2,7 +2,7 @@ from flask import jsonify
 from sqlalchemy import or_, func
 
 import config
-from app import db
+from app import db, dec64
 from app.api import bp
 from app.models import Modelgood, Storage, Vollink, Vol, Folder
 from config import Config
@@ -16,8 +16,8 @@ def get_model_by_id(id):
 @bp.route('/modelgoods/search/<string:search_text>', methods=['GET'])
 def get_models_by_id(search_text):
     if len(search_text) > 3:
-        base_query = db.session.query(func.sum(Storage.count),func.max(Storage.p2value), func.max(Modelgood.name),func.max(Modelgood.id),
-            func.max(Vollink.barcode),func.max(Vollink.codemodel),func.max(Vol.name).label("Volname"), func.max(Folder.name).label('Foldername')).\
+        base_query = db.session.query(func.sum(Storage.count),func.max(Storage.p2value), func.max(Modelgood.name),func.max(Modelgood.id),func.max(Modelgood.imgext),
+            func.max(Vollink.barcode),func.max(Vollink.codemodel),func.max(Vol.name).label("Volname"), func.list(Folder.name).label('Foldername')).\
             join(Modelgood, Storage.modelid == Modelgood.id).\
             join(Folder, Storage.folderid == Folder.id).\
             join(Vollink, Modelgood.id == Vollink.modelid).join(Vol, Vollink.vol1id == Vol.id).group_by(Modelgood.id).\
@@ -33,6 +33,11 @@ def get_models_by_id(search_text):
             if len(search_text) > 3:
                 search_args = [col.ilike('%%%s%%' % search_text) for col in [Modelgood.name, Vollink.barcode]]
                 base_query = base_query.filter(or_(*search_args))
-        list_to_json = base_query.paginate(1,Config.MODELGOODS_PER_PAGE,
-                                            False).items
+        list_to_json = [{"count":sc,"price":sp, 'name':mn, 'id':mi, 'barcde':vb,'code':vc,'volname':vn,"foldername":fn,
+                        "img_url": "http://" + config.Config.serverdb + '''/img/''' + (dec64(mi)+'.'+mimext) if
+                        mimext else None}
+                        for sc,sp, mn, mi,mimext, vb,vc,vn,fn in
+                        base_query.paginate(1,
+                                            Config.MODELGOODS_PER_PAGE,
+                                            False).items]
         return jsonify(list_to_json)
